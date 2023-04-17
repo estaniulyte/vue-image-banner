dde076b7ce916fd7f14eac2fcbb8a9f63fd51fc1
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-04-17 16:06:49 +0300

    feat: add VueWriter package

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import router from "./router";
 
 import "./assets/main.css";
 
+import VueWriter from "vue-writer";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.use(VueWriter)
 
 app.mount("#app");
